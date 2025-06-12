0bb61ac1af4f74c4bf0a2d222335c7d2023ceea9
lucasb08 <349997429@gapps.yrdsb.ca>
2025-06-11 21:16:25 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,187 +5,209 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         // adding all array lists for service necessities
         ArrayList<String> serviceNames = new ArrayList<>();
         ArrayList<String> serviceCategories = new ArrayList<>();
         ArrayList<Double> serviceRates = new ArrayList<>();
         ArrayList<String> cart = new ArrayList<>();
-        
+        ArrayList<Double> ratesforbooking = new ArrayList<>();
+
         int counter = 0;
-        
+
         // introduction
-        
+
         System.out.println("Welcome to ABay!");
-        
+
         // choose a service
         boolean isRunning = true;
-        
+
         while(isRunning) { // while loop to exit program
-        System.out.println("Add a service (1).");
-        System.out.println("Book a service (2).");
-        System.out.println("Edit your cart/checkout. (3).");
-        System.out.println("Exit (4).");
-        int options = sc.nextInt();
-        
-        sc.nextLine(); // fixes skipping issue
+            System.out.println("Add a service (1).");
+            System.out.println("Book a service (2).");
+            System.out.println("Edit your cart/checkout. (3).");
+            System.out.println("Exit (4).");
+            int options = sc.nextInt();
 
-        if(options == 1) { // if user chooses to add a service
-        boolean run = true;
+            sc.nextLine(); // fixes skipping issue
 
-            while(run) {
-                System.out.println("Enter the service's name: ");
-                serviceNames.add(sc.nextLine());
+            if(options == 1) { // if user chooses to add a service
+                boolean run = true;
 
-                System.out.println("Enter your service's category: ");
-                serviceCategories.add(sc.nextLine());
+                while(run) {
+                    System.out.println("Enter the service's name: ");
+                    serviceNames.add(sc.nextLine());
 
-                System.out.println("Enter your hourly rate: ");
-                serviceRates.add(sc.nextDouble());
-                sc.nextLine(); // fixes skipping issue
+                    System.out.println("Enter your service's category: ");
+                    serviceCategories.add(sc.nextLine());
 
-                counter++;
+                    System.out.println("Enter your hourly rate: ");
+                    serviceRates.add(sc.nextDouble());
+                    sc.nextLine(); // fixes skipping issue
 
-                System.out.println("Your service has been added!");
+                    counter++;
 
-                System.out.println("Would you like to add another service? (yes/no)");
+                    System.out.println("Your service has been added!");
 
-                String addService = sc.nextLine();
+                    System.out.println("Would you like to add another service? (yes/no)");
 
-                if (addService.equals("no")) {
-                run = false;
+                    String addService = sc.nextLine();
+
+                    if (addService.equals("no")) {
+                        run = false;
+                    }
                 }
+            } else if(options == 2) { // if user chooses to book a service
+
+                System.out.println("Select one of the categories to view services within it: ");
+                System.out.println("1. Cleaning. (1).");
+                System.out.println("2. Tutoring. (2).");
+                System.out.println("3. Childcare. (3).");
+                int optionsBooking = sc.nextInt();
+
+                switch(optionsBooking) {
+                    case (1):
+                        System.out.println("1. Name: Ludo's Cleaning Services. (1).");
+                        System.out.println("Hourly Rate: $25.00/hour.");
+                        System.out.println("2. Name: Clean Floors inc. (2).");
+                        System.out.println("Hourly Rate: $22.00/hour.");
+                        System.out.println("3. Name: Smiling Surfaces Cleaning. (3).");
+                        System.out.println("Hourly Rate: $20.00/ hour.");
+
+                        int optionsCleaning = sc.nextInt();
+
+                        if(optionsCleaning == 1) {
+                            cart.add("Ludo's Cleaning Services");
+                            System.out.println("Your order has been added to your cart.");
+                            ratesforbooking.add(25.00);
+
+                        } else if(optionsCleaning == 2) {
+                            cart.add("Clean Floors inc.");
+                            System.out.println("Your order has been added to your cart.");
+                            ratesforbooking.add(22.00);
+
+                        } else if(optionsCleaning == 3) {
+                            cart.add("Smiling Surfaces Cleaning");
+                            System.out.println("Your order has been added to your cart.");
+                            ratesforbooking.add(20.00);
+
+                        } else {
+                            System.out.println("Invalid input please try again.");
+
+                        }
+                        System.out.println("Your cart: " + (cart));
+                        break;
+
+
+                    case (2):
+                        System.out.println("1. Name: Marcus' Tutoring Classes. (1).");
+                        System.out.println("Hourly Rate: $17.50/hour.");
+                        System.out.println("2. Name: A+ Tutoring & Learning. (2).");
+                        System.out.println("Hourly Rate: $19.00/hour.");
+                        System.out.println("3. Name: Top Tier Tutoring. (3).");
+                        System.out.println("Hourly Rate: $18.00/hour.");
+                        int optionsTutoring = sc.nextInt();
+
+                        if(optionsTutoring == 1) {
+                            cart.add("Marcus' Tutoring Classes");
+                            System.out.println("Your order has been added to your cart.");
+                            ratesforbooking.add(17.50);
+
+                        } else if(optionsTutoring == 2) {
+                            cart.add("A+ Tutoring & Learning");
+                            System.out.println("Your order has been added to your cart.");
+                            ratesforbooking.add(19.00);
+
+                        } else if(optionsTutoring == 3) {
+                            cart.add("Top Tier Tutoring");
+                            System.out.println("Your order has been added to your cart.");
+                            ratesforbooking.add(18.00);
+
+                        } else {
+                            System.out.println("Inalid input, please try again.");
+                        }
+                        System.out.println("Your cart: " + (cart));
+
+                        break;
+
+
+                    case (3):
+                        System.out.println("1. Name: Betty's Babysitting. (1).");
+                        System.out.println("Hourly Rate: $20.50/hour.");
+                        System.out.println("2. Name: Brandon's Childcare Services. (2).");
+                        System.out.println("Hourly Rate: $21.00/hour.");
+                        System.out.println("3. Name: Babysitting & Nannies inc. (3).");
+                        System.out.println("Hourly Rate: $30.00/hour.");
+                        int optionsChildcare = sc.nextInt();
+
+                        if(optionsChildcare == 1) {
+                            cart.add("Betty's Babysitting");
+                            System.out.println("Your order has been added to your cart.");
+                            ratesforbooking.add(20.50);
+
+                        } else if(optionsChildcare == 2) {
+                            cart.add("Brandon's Childcare Services");
+                            System.out.println("Your order has been added to your cart.");
+                            ratesforbooking.add(21.00);
+
+                        } else if(optionsChildcare == 3) {
+                            cart.add("Babysitting & Nannies inc.");
+                            System.out.println("Your order has been added to your cart.");
+                            ratesforbooking.add(30.00);
+
+                        } else {
+                            System.out.println("Invalid input, please try again.");
+                        }
+                        System.out.println("Your cart: " + (cart));
+
+                        break;
+
+
+                    default:
+                        System.out.println("Invalid input, please try again.");
+                        break;
+                }
+
+            } else if(options == 3) { // if user chooses to exit program
+                System.out.println("Would you like to: ");
+                System.out.println("View your cart. (1).");
+                System.out.println("Remove items from your cart. (2).");
+                System.out.println("Checkout. (3).");
+                int optionsCheckout = sc.nextInt();
+
+                if(optionsCheckout == 1) {
+
+                    System.out.println(cart);
+
+                } else if(optionsCheckout == 2) {
+
+                    System.out.println("Which item would you like to remove from your cart?");
+                    System.out.println(cart);
+                    System.out.println("Enter the position the item is in to remove it.");
+                    int remove = sc.nextInt();
+                    cart.remove(remove - 1);
+                    System.out.println("Updated cart: " + (cart));
+
+                } else if(optionsCheckout == 3) {
+
+                    System.out.println("Invoice: ");
+                    int counter3 = 0;
+                    for(String i : cart) {
+                        System.out.println(i + " Hourly Rate: $" + ratesforbooking.get(counter3) + " per hour.");
+                        counter3++;
+                    }
+                    System.out.print("Your total is: ");
+                    double totalSum = 0;
+                    for(Double i : ratesforbooking) {
+                        totalSum += i;
+                    }
+                    System.out.println("$" + totalSum);
+                    System.out.println("Thank you for shopping with ABay.");
+                }
+            } else if(options == 4) {
+                System.out.println("Thank you for using ABay, come again soon.");
+                isRunning = false;
             }
-        } else if(options == 2) { // if user chooses to book a service
-        	
-            System.out.println("Select one of the categories to view services within it: ");
-            System.out.println("1. Cleaning. (1).");
-            System.out.println("2. Tutoring. (2).");
-            System.out.println("3. Childcare. (3).");
-            int optionsBooking = sc.nextInt();
-            int counter2 = 0;
-            
-            switch(optionsBooking) {
-            case (1):
-            	System.out.println("1. Name: Ludo's Cleaning Services. (1).");
-            	System.out.println("Hourly Rate: $25.00/hour.");
-            	System.out.println("2. Name: Clean Floors inc. (2).");
-            	System.out.println("Hourly Rate: $22.00/hour.");
-            	System.out.println("3. Name: Smiling Surfaces Cleaning. (3).");
-            	System.out.println("Hourly Rate: $20.00/ hour.");
-            	
-            	int optionsCleaning = sc.nextInt();
-            	
-            	if(optionsCleaning == 1) {
-            		cart.add("Ludo's Cleaning Services");
-            		System.out.println("Your order has been added to your cart.");
-            		counter2++;
-            	} else if(optionsCleaning == 2) {
-            		cart.add("Clean Floors inc.");
-            		System.out.println("Your order has been added to your cart.");
-            		counter2++;
-            	} else if(optionsCleaning == 3) {
-            		cart.add("Smiling Surfaces Cleaning");
-            		System.out.println("Your order has been added to your cart.");
-            		counter2++;
-            	} else {
-            		System.out.println("Invalid input please try again.");
-            	}
-            	System.out.println("Your cart: " + (cart));
-            	break; 
-            	
-            	
-            case (2):
-            	System.out.println("1. Name: Marcus' Tutoring Classes. (1).");
-            	System.out.println("Hourly Rate: $17.50/hour.");
-            	System.out.println("2. Name: A+ Tutoring & Learning. (2).");
-            	System.out.println("Hourly Rate: $19.00/hour.");
-            	System.out.println("3. Name: Top Tier Tutoring. (3).");
-            	System.out.println("Hourly Rate: $18.00/hour.");
-            	int optionsTutoring = sc.nextInt();
-            	
-            	if(optionsTutoring == 1) {
-            		cart.add("Marcus' Tutoring Classes");
-            		System.out.println("Your order has been added to your cart.");
-            		counter2++;
-            	} else if(optionsTutoring == 2) {
-            		cart.add("A+ Tutoring & Learning");
-            		System.out.println("Your order has been added to your cart.");
-            		counter2++;
-            	} else if(optionsTutoring == 3) {
-            		cart.add("Top Tier Tutoring");
-            		System.out.println("Your order has been added to your cart.");
-            		counter2++;
-            	} else {
-            		System.out.println("Inalid input, please try again.");
-            	}
-            	System.out.println("Your cart: " + (cart));
-            	
-            	break;
-            	
-            	
-            case (3):
-            	System.out.println("1. Name: Betty's Babysitting. (1).");
-            	System.out.println("Hourly Rate: $20.50/hour.");
-            	System.out.println("2. Name: Brandon's Childcare Services. (2).");
-            	System.out.println("Hourly Rate: $21.00/hour.");
-            	System.out.println("3. Name: Babysitting & Nannies inc. (3).");
-            	System.out.println("Hourly Rate: $30.00/hour.");
-            	int optionsChildcare = sc.nextInt();
-            	
-            	if(optionsChildcare == 1) {
-            		cart.add("Betty's Babysitting");
-            		System.out.println("Your order has been added to your cart.");
-            		counter2++;
-            	} else if(optionsChildcare == 2) {
-            		cart.add("Brandon's Childcare Services");
-            		System.out.println("Your order has been added to your cart.");
-            		counter2++;
-            	} else if(optionsChildcare == 3) {
-            		cart.add("Babysitting & Nannies inc.");
-            		System.out.println("Your order has been added to your cart.");
-            		counter2++;
-            	} else {
-            		System.out.println("Invalid input, please try again.");
-            	}
-            	System.out.println("Your cart: " + (cart));
-            	
-            	break;
-            	
-            	
-            	default:
-            		System.out.println("Invalid input, please try again.");
-            		break;
-            }
-            
-        } else if(options == 3) { // if user chooses to exit program
-        	System.out.println("Would you like to: ");
-        	System.out.println("View your cart. (1).");
-        	System.out.println("Remove items from your cart. (2).");
-        	System.out.println("Checkout. (3).");
-        	int optionsCheckout = sc.nextInt();
-        	
-        	if(optionsCheckout == 1) {
-        		
-        		System.out.println(cart);
-        		
-        	} else if(optionsCheckout == 2) {
-        		
-        		System.out.println("Which item would you like to remove from your cart?");
-        		System.out.println(cart);
-        		System.out.println("Enter the position the item is in to remove it.");
-        		int remove = sc.nextInt();
-        		cart.remove(remove - 1);
-        		System.out.println("Updated cart: " + (cart));
-        		
-        	} else if(optionsCheckout == 3) {
-        		
-        		System.out.println("Invoice: ");
-        		for(int i = 0; i < cart.size() + 1; i++) {
-        			System.out.println(cart.get(i) + ": Hourly rate: " + serviceRates.get(i));
-        		}
-        	}
-        }
         }
     }
 }
